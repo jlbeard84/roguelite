dee9ee8efa086c2a1a7ccdf54c9e136221146b3f
Josh Beard <josh@jlbeard.com>
2017-10-27 09:29:52 -0400

    Added base class for game scenes.

---
+++ src/scenes/game-scene.base.ts
@@ -0,0 +1,15 @@
+import { Actor, Color, LockedCamera, Scene, TileMap, TileSprite } from "excalibur";
+
+import { Hero } from "../actors";
+
+export abstract class GameSceneBase extends Scene {
+    public sceneName: string;
+    public hero: Hero;
+
+    constructor(sceneName: string, hero: Hero) {
+        super();
+
+        this.sceneName = sceneName;
+        this.hero = hero;
+    }
+}
--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -1,1 +1,2 @@
 export { CaveScene } from "./cave.scene";
+export { GameSceneBase } from "./game-scene.base";
