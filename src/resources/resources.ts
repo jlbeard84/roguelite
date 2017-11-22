4f8fa5b42940d45b0e499b9206a6b4fd00a8dbf6
Josh Beard <josh@jlbeard.com>
2017-11-21 23:57:39 -0500

    Added character attack spritesheet.

--- src/resources/resources.ts
+++ src/resources/resources.ts
@@ -4,12 +4,14 @@ export class Resources {
 
     public CaveMapSheet: Texture = new Texture("/assets/cave.png");
     public CharacterSheet: Texture = new Texture("/assets/character.png");
+    public CharacterAttackSheet: Texture = new Texture("/assets/character-attack.png");
     public CreepSheet: Texture = new Texture("/assets/creep.png");
     public GameFontSheet: Texture = new Texture("/assets/game-font.png");
 
     private resources: ILoadable[] = [
         this.CaveMapSheet,
         this.CharacterSheet,
+        this.CharacterAttackSheet,
         this.CreepSheet,
         this.GameFontSheet
     ]
